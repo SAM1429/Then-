79114464c92a0bf6cdd3b16fb4d77bb0bfa180d3
SamyukthaSridhar2014 <samyuktha.sridhar.2014@gmail.com>
2024-04-14 19:09:37 +0530

    signup page is done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-
+import {errorHandler} from "../utils/error.js"
+ 
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
 
@@ -11,7 +12,6 @@ export const signup = async (req, res,next) => {
 
   const newUser = new User({ username, email, password: hashPassword });
   try {
-   
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
--- collabStory/src/pages/SignUp.jsx
+++ collabStory/src/pages/SignUp.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Label, TextInput } from "flowbite-react";
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="min-h-screen mt-20 text-center">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white text-center"
+      >
+        <span className="px-2 py-1 bg-indigo-400 text-white rounded-lg">
+          Then?
+        </span>
+      </Link>
+      <p className="p-4 font-semibold">
+        Welcome to the coolest collaborative story writing community out there,
+        SignUp below to get started!
+      </p>
+      <div className="flex justify-center">
+        <form className="w-full max-w-md">
+          <div className="flex flex-col gap-4">
+            <Label value="Your username" />
+            <TextInput type="text" placeholder="Username" id="username" />
+          </div>
+          <div className="flex flex-col gap-4">
+            <Label value="Your email" />
+            <TextInput type="text" placeholder="Email" id="email" />
+          </div>
+          <div className="flex flex-col gap-4">
+            <Label value="Your password" />
+            <TextInput type="text" placeholder="Password" id="password" />
+          </div>
+          <br />
+          <Button
+            color="gray"
+            className="w-full hover:bg-blend-color-burn"
+            type="submit"
+          >
+            Sign Up
+          </Button>
+        </form>
+      </div>
+      <div className="p-2">
+        <span >Already have an account?</span>
+        <Link to="/signin" className="text-indigo-400">
+          Sign In
+        </Link>
+      </div>
+    </div>
+  );
 }
